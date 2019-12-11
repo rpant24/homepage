1d1520e7726cbe057163728be917cc6dcf932f86
Aniket Pant <pant.aniket@gmail.com>
2019-12-10 18:52:38 -0600

    Updates

---
+++ precache-manifest.5fbb6da419492f5d4e814ce995ae6db1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6360191d30861403351d427c2866d1",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "6d53a3aa582c821e46e2",
+    "url": "/homepage/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "ac65d5ab9627f9f8d65d",
+    "url": "/homepage/static/js/2.b2c95304.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/homepage/static/js/2.b2c95304.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d53a3aa582c821e46e2",
+    "url": "/homepage/static/js/main.9842629b.chunk.js"
+  },
+  {
+    "revision": "1376ffcfdf543a7e02fe",
+    "url": "/homepage/static/js/runtime-main.c048e2d4.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/homepage/static/media/logo.ee7cd8ed.svg"
+  }
+]);
